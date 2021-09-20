cd16765f3802aeb44e6ad32f497b4d3db49d1918
Bün Colak <bun.colak@motius.de>
2021-09-20 17:00:07 +0200

    footer

---
+++ src/components/LatestPosts.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let posts;
+
+  $: sortedPosts = posts
+    .sort((p1, p2) => new Date(p2).getDate() - new Date(p1).getDate())
+    .slice(0, 3);
+</script>
+
+<div>
+  <h2 class="my-16 text-5xl text-center text-bold">Latest Posts</h2>
+  <div class="relative h-24 overflow-hidden">
+    <div class="absolute top-0 left-0 flex h-24 items w-[300%]">
+      {#each sortedPosts as post}
+        <div class="w-[10%]">
+          <a class="text-lg" href={post.url}>{post.title}</a>
+        </div>
+      {/each}
+      {#each sortedPosts as post}
+        <div class="w-[10%]">
+          <a class="text-lg" href={post.url}>{post.title}</a>
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
+
+<style>
+  .items {
+     animation: slideshow 30s linear infinite
+  }
+
+  @keyframes slideshow {
+    0% {
+      left: 0;
+    }
+    100% {
+      left: -90%;
+    }
+  }
+</style>
--- src/components/ProjectsOverview.svelte
+++ src/components/ProjectsOverview.svelte
@@ -1,5 +1,5 @@
 <div class="container mx-auto">
-  <h2 class="my-4 text-5xl font-bold lg:text-center">My work</h2>
+  <h2 class="my-4 mt-16 text-5xl font-bold lg:text-center">My work</h2>
   <div class="grid gap-16 px-4 mt-16 lg:grid-cols-2">
     <div>
       <figure>
